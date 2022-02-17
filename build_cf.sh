bce1eb7fa3d821f9e874cfa58de662f95ee39cf5
panda <zhonghaoyu555@gmail.com>
2022-02-17 13:19:29 +0800

    another try

--- build_cf.sh
+++ build_cf.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 echo "clone all history for .GitInfo...\n\r"
-git pull --unshallow
-git checkout pages-src
-git pull origin pages-src
-
+git clone https://github.com/BlurryLight/BlurryLight_blog.git
+cd BlurryLight_blog
 ls -al
+
 echo "build hugo\n\r"
-chmod +x hugo
-./hugo --enableGitInfo
+chmod +x ../hugo
+../hugo --enableGitInfo 
+mv public ../public
 
 exit 0
