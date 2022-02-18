74527868a0584b363c2d1aa9feb609816faf5a7c
panda <zhonghaoyu555@gmail.com>
2022-02-18 15:01:15 +0800

    update github

--- build_cf.sh
+++ build_cf.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+
 echo "clone all history for .GitInfo...\n\r"
-git clone https://github.com/BlurryLight/BlurryLight_blog.git
-cd BlurryLight_blog
+git pull --unshallow
+git checkout pages-src
+git pull origin pages-src
 ls -al
 
+echo "Set git config"
+git config core.quotePath false
+
 echo "build hugo\n\r"
-chmod +x ../hugo
-../hugo --enableGitInfo 
-mv public ../public
+chmod +x hugo
+./hugo --enableGitInfo
 
 exit 0
